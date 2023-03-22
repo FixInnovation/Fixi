6b44bd09a0df28bc684ca15d63a66fb5d9e13c90
Fix Innovation <104660098+FixInnovation@users.noreply.github.com>
2023-03-21 22:11:32 -0600

    Add files via upload

---
+++ js/info.js
@@ -0,0 +1,6 @@
+console.info("%c¡Detente!",
+            "color:#f21C0A; font-size: 50px; font-weight: 800;");
+
+
+console.log("%cEsta función del navegador está pensada para desarrolladores. Si alguien te indicó que copiaras y pegaras algo aquí para habilitar una función de Enginet o para 'hackear' la cuenta de alguien, se trata de un fraude. Si lo haces, esta persona podrá acceder a tu cuenta.",
+"color:#fff; font-size: 17px; font-weight: 500; line-height: 23px; font-family: Segoe UI;")
